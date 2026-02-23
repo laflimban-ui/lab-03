8cc98b976fa6fc497cb063a20f16831c54d27edb
Lana Filmban <laflimban@gmail.com>
2026-02-23 12:48:46 +0300

    completed lab3

--- src/main/java/edu/kau/fcit/cpit252/App.java
+++ src/main/java/edu/kau/fcit/cpit252/App.java
@@ -6,7 +6,7 @@ public class App {
         // print car and recall info
         System.out.println(c1);
         // create a duplicate car object to print the recall info in a table
-        Car c2 = new Car("Honda", "Accord", 2019);
+        Car c2 = c1.clone();
         RecallViewer rv = new RecallViewer(c2);
         rv.printTableView();
     }
